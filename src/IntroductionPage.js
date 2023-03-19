f25daee1807b157e66d4c790e956b591eeb4a35c
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-19 15:23:03 +0000

    added CSS
    still need to fix content on introduction page

--- src/IntroductionPage.js
+++ src/IntroductionPage.js
@@ -7,32 +7,39 @@ export default function () {
     <div className="IntroductionPage">
       <div className="half-a-page-grid">
         <div className="introduction-half-page-image">
-          <h3 className="white-text"> Fresh n’ Tasty !</h3>
-          <p className="secondary-text-color subHeading">
-            All our fish is cooked to order, this will ensure the fish tastes as
-            crispy & light as possible.
-          </p>
+          <div className="section-padding">
+            <h3 className="white-text largeHeading"> Fresh n’ Tasty !</h3>
+            <p className="secondary-text-color itallic-subHeading">
+              All our fish is cooked to order, this will ensure the fish tastes
+              as crispy & light as possible.
+            </p>
+          </div>
         </div>
         <div className="half-a-page-content">
-          <h2>Must Try</h2>
-          <h3>Our Specialty Homemade Curry Sauce. </h3>
-          <p>
-            A perfect accompaniment with chips (sprinkle of salt and vinegar on
-            the chips) and is suitable for vegetarians.
-          </p>
-          <div className="image-grid">
-            <img
-              className="sqaure-image"
-              src={fishNChipsImage}
-              alt="fish-chips"
-            ></img>
-            <img
-              className="sqaure-image"
-              src={pieNSauceImage}
-              alr="pieNSauce"
-            ></img>
+          <div className="section-padding">
+            <h2 className="black-large-title">Must Try</h2>
+            <h3 className="black-subtitle">
+              Our Specialty Homemade Curry Sauce.{" "}
+            </h3>
+            <p>
+              A perfect accompaniment with chips (sprinkle of salt and vinegar
+              on the chips) and is suitable for vegetarians.
+            </p>
+            <div className="image-grid">
+              <img
+                className="sqaure-image"
+                src={fishNChipsImage}
+                alt="fish-chips"
+              ></img>
+              <img
+                className="sqaure-image"
+                src={pieNSauceImage}
+                alr="pieNSauce"
+              ></img>
+            </div>
+            <h2 className="black-large-title">Order Now</h2>
+            <OrderGrid textColor="black" />
           </div>
-          <OrderGrid textColor="black" />
         </div>
       </div>
     </div>
