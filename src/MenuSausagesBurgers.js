c7a5c6c2208cf2f2c734d70132881ce43e22bee5
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-23 12:56:56 +0000

    Update MenuSausagesBurgers.js

--- src/MenuSausagesBurgers.js
+++ src/MenuSausagesBurgers.js
@@ -13,6 +13,17 @@ export default function MenuSausagesBurgers(props) {
               food="Battered"
               price={["Small £1.30 ", "Large £2"]}
             />
+            <MenuItemGrid food="Saveloy" price={["£1.90"]} />
+            <MenuItemGrid food="Bigfoot" price={["£4.10"]} />
+            <small>*served in a baguette*</small>
+            <MenuItemGrid food="Giant battered sausage" price={["£2"]} />
+          </div>
+          <div className="menu-card">
+            <h3 className="itallic-subHeading">Burgers</h3>
+            <MenuItemGrid
+              food="Beef burger"
+              price={["Small £2.80 ", "Large £3.80"]}
+            />
           </div>
         </div>
       </div>
