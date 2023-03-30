768fbd327070c3a901b0d10035bcb4a86b7da132
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-30 14:18:04 +0100

    made sausage menu prices variables

--- src/MenuSausage.js
+++ src/MenuSausage.js
@@ -1,17 +1,48 @@
 import React from "react";
 import MenuItemGrid from "./MenuItemGrid";
 export default function MenuSausage(props) {
+  const plainPrices = [
+    { size: "Small", price: 1.2 },
+    { size: "Large", price: 1.9 },
+  ];
+  const batteredPrices = [
+    { size: "Small", price: 1.3 },
+    { size: "Large", price: 2 },
+  ];
+  const saveloyPrice = [{ price: 1.9 }];
+  const bigfootPrice = [{ price: 4.1 }];
+  const giantBatteredSausagePrice = [{ price: 2 }];
   return (
     <div className="MenuSausage" id={props.menuDetails.menuHref}>
       <div className="section-padding">
         <div className="menu-card">
           <h3 className="itallic-subHeading">{props.menuDetails.menuTitle}</h3>
-          <MenuItemGrid food="Plain" price={["Small £1.20", "Large £1.90"]} />
-          <MenuItemGrid food="Battered" price={["Small £1.30 ", "Large £2"]} />
-          <MenuItemGrid food="Saveloy" price={["£1.90"]} />
-          <MenuItemGrid food="Bigfoot" price={["£4.10"]} />
+          <MenuItemGrid
+            food="Plain sausage"
+            price={plainPrices}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Battered sausage"
+            price={batteredPrices}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Saveloy"
+            price={saveloyPrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Bigfoot"
+            price={bigfootPrice}
+            button={props.button}
+          />
           <small>*served in a baguette*</small>
-          <MenuItemGrid food="Giant battered sausage" price={["£2"]} />
+          <MenuItemGrid
+            food="Giant battered sausage"
+            price={giantBatteredSausagePrice}
+            button={props.button}
+          />
         </div>
       </div>
     </div>
