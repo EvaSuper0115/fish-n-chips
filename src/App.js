0ad24c01531682f47943cea9ca568760b2da14f4
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-30 12:37:13 +0100

    deleted usestate on app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useState } from "react";
 import PageHero from "./PageHero";
 import IntroductionPage from "./IntroductionPage";
 import OpeningHours from "./OpeningHours";
