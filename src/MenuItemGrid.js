2f5802519cea66637392da391ebff35ecee606fd
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-31 15:03:27 +0100

    made note to a table

--- src/MenuItemGrid.js
+++ src/MenuItemGrid.js
@@ -3,15 +3,27 @@ import Note from "./Note";
 import SaveButton from "./SaveButton";
 export default function MenuItemGrid(props) {
   let button = <button className="highlight-background">Save</button>;
-  const [note, setNote] = useState("");
+  const [note, setNote] = useState(<div></div>);
   const [addedFood, setAddedFood] = useState("");
   const [addedPrice, setAddedPrice] = useState(null);
   useEffect(() => {
     if (addedFood !== "") {
-      setNote(`saved 
-      ${addedFood} - £${Number(addedPrice).toFixed(2)} to note`);
+      setNote(
+        <div>
+          <h4 className="note-title">My notes</h4>
+          <table>
+            <thead>
+              <tr>
+                <th className="note-items">{addedFood}</th>
+                <th className="note-items">£{Number(addedPrice).toFixed(2)}</th>
+              </tr>
+            </thead>
+          </table>
+          <h4 className="note-title">order no. 07578617349</h4>
+        </div>
+      );
     }
-  });
+  }, [addedFood, addedPrice]);
 
   function handleAdd(event, price) {
     event.preventDefault();
