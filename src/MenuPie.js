3b65ba19c7d78890030f75469e8ff096bcc7bd99
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-31 12:18:19 +0100

    made Pie prices to variables

--- src/MenuPie.js
+++ src/MenuPie.js
@@ -1,15 +1,28 @@
 import React from "react";
 import MenuItemGrid from "./MenuItemGrid";
 export default function MenuPie(props) {
+  const piePrice = [{ price: 3.8 }];
   return (
     <div className="MenuPie" id={props.menuDetails.menuHref}>
       <div className="section-padding">
         <div className="menu-card">
           <h3 className="itallic-subHeading">{props.menuDetails.menuTitle}</h3>
 
-          <MenuItemGrid food="Chicken & mushroom" price={["£3.80"]} />
-          <MenuItemGrid food="Steak & kidney" price={["£3.80"]} />
-          <MenuItemGrid food="Beef & onion" price={["£3.80"]} />
+          <MenuItemGrid
+            food="Chicken & mushroom pie"
+            price={piePrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Steak & kidney pie"
+            price={piePrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Beef & onion pie"
+            price={piePrice}
+            button={props.button}
+          />
         </div>
       </div>
     </div>
