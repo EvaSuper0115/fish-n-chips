36038127b75120b4ff3ced2014ffd36f56089432
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-31 12:21:21 +0100

    made menuChicken prices to variables

--- src/MenuChicken.js
+++ src/MenuChicken.js
@@ -1,20 +1,37 @@
 import React from "react";
 import MenuItemGrid from "./MenuItemGrid";
 export default function MenuChicken(props) {
+  const quarterBreastPrice = [{ price: 5 }];
+  const nuggetsPrice = [{ price: 4.6 }];
+  const popcornChickenPrice = [{ price: 2.7 }];
+  const SpicyChickenWingsPrice = [{ price: 2.7 }];
   return (
     <div className="MenuChicken" id={props.menuDetails.menuHref}>
       <div className="section-padding">
         <div className="menu-card">
           <h3 className="itallic-subHeading">{props.menuDetails.menuTitle}</h3>
 
-          <MenuItemGrid food="Quarter breast" price={["£5.00"]} />
-          <MenuItemGrid food="Nuggets (8)" price={["£4.60"]} />
+          <MenuItemGrid
+            food="Quarter breast"
+            price={quarterBreastPrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Nuggets (8)"
+            price={nuggetsPrice}
+            button={props.button}
+          />
 
-          <MenuItemGrid food="Popcorn chicken" price={["£2.70"]} />
+          <MenuItemGrid
+            food="Popcorn chicken"
+            price={popcornChickenPrice}
+            button={props.button}
+          />
           <MenuItemGrid
             food="
 Spicy chicken wings (4)"
-            price={["£2.70"]}
+            price={SpicyChickenWingsPrice}
+            button={props.button}
           />
         </div>
       </div>
