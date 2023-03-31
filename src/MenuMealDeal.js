f649f898272c17bab8ae9a8eb292763c7c19d5cb
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-31 13:03:57 +0100

    turned all menu items to variables

--- src/MenuMealDeal.js
+++ src/MenuMealDeal.js
@@ -1,16 +1,41 @@
 import React from "react";
 import MenuItemGrid from "./MenuItemGrid";
 export default function MenuMealDeal(props) {
+  const mealDealCodChipsPrice = [{ price: 7 }];
+  const mealDealCheeseBurgerChipsPrice = [{ price: 5.9 }];
+  const mealDealChickenNuggetChipsPrice = [{ price: 6.2 }];
+  const mealDealLargeSuasageChipsPrice = [{ price: 4.7 }];
+  const mealDealFishcakeChipsPrice = [{ price: 4.2 }];
   return (
     <div className="MenuOthers" id={props.menuDetails.menuHref}>
       <div className="section-padding">
         <div className="menu-card">
           <h3 className="itallic-subHeading">{`${props.menuDetails.menuTitle} (including a canned drink)`}</h3>
-          <MenuItemGrid food="Cod & chips" price={["£7"]} />
-          <MenuItemGrid food="Cheese burger & chips" price={["£5.90"]} />
-          <MenuItemGrid food="Chicken nuggets & chips" price={["£6.20"]} />
-          <MenuItemGrid food="Large Sausage & chips" price={["£4.70"]} />
-          <MenuItemGrid food="Fishcake & chips" price={["£4.20"]} />
+          <MenuItemGrid
+            food="Cod & chips"
+            price={mealDealCodChipsPrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Cheese burger & chips"
+            price={mealDealCheeseBurgerChipsPrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Chicken nuggets & chips"
+            price={mealDealChickenNuggetChipsPrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Large Sausage & chips"
+            price={mealDealLargeSuasageChipsPrice}
+            button={props.button}
+          />
+          <MenuItemGrid
+            food="Fishcake & chips"
+            price={mealDealFishcakeChipsPrice}
+            button={props.button}
+          />
         </div>
       </div>
     </div>
--- src/MenuOAP.js
+++ src/MenuOAP.js
@@ -1,12 +1,17 @@
 import React from "react";
 import MenuItemGrid from "./MenuItemGrid";
 export default function MenuOAP(props) {
+  const OAPprice = [{ price: 5.7 }];
   return (
     <div className="MenuOthers" id={props.menuDetails.menuHref}>
       <div className="section-padding">
         <div className="menu-card">
           <h3 className="itallic-subHeading">{`${props.menuDetails.menuTitle} `}</h3>
-          <MenuItemGrid food="£5.70" price={[""]} />
+          <MenuItemGrid
+            food="OAP cod & chips"
+            price={OAPprice}
+            button={props.button}
+          />
         </div>
       </div>
     </div>
